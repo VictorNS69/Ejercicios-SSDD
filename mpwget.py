b28ae422b4e763c880f0b8fac27feab0e46a49b6
Victor Nieves Sanchez <vnievess@gmail.com>
2019-03-02 22:04:34 +0100

    updated code. It isn't finished

--- mpwget.py
+++ mpwget.py
@@ -1,46 +1,104 @@
-import argparse
+import sys
+import requests
 
 
-def possible_server(args):
+def parser():
     """
-    If the args starts with : it is a possible server
-    """
-    return str(args).startswith(":")
-
-
-def parsing():
-    """
-    Parser
+    Parses the arguments
     @:return returns the dictionary with objects and servers
     """
-    parser = argparse.ArgumentParser(prog="mpwget")
-
-    # At least 1 object
-    parser.add_argument("object", nargs="+", help="the objects to get")
-    # At least 1 server
-    parser.add_argument("server", nargs="+", help="the servers to search")
-
-    args = parser.parse_args()
-
-    # Initialize an empty dictionary with objects and servers (lists)
-    d = {"objects": [], "servers": []}
-
-    for l in args.object, args.server:
-        for item in l:
-            if possible_server(item):
-                d["servers"].append(item)
-            else:
-                d["objects"].append(item)
-
-    return d
+    # TODO: empty parameters --> usage message
+    # TODO: it must have at least 1 object and 1 server
+    arg = {"objects": [], "servers": []}
+    for a in sys.argv[1:]:
+        if a[0] == ':':
+            arg["servers"].append(a[1:])
+        else:
+            arg["objects"].append(a)
+    return arg
 
 
-def main():
-    args = parsing()
-    print("objects:", args["objects"])
-    print("servers:", args["servers"])
+def ping_alive(arg):
+    """
+    Modifies the input dictionary with the alive servers
+    :param arg: dictionary with objects and servers
+    :return: The same dictionary with only alive servers
+    """
+    for server in arg["servers"]:
+        try:
+            req = requests.head(server)
+            if not req.status_code == 200:
+                arg["servers"].remove(server)
+        except Exception:
+            arg["servers"].remove(server)
+    # print("arg in ping", arg)
+    return arg
+
+
+def get_sizes(args):
+    """
+    Calculates the size of every object
+    @:param args: dictionary of objects and servers
+    @:return Returns the size for every object
+    """
+    sizes = {}
+    for server in args["servers"]:
+        for obj in args["objects"]:
+            req = requests.head("%s/%s" % (server, obj))
+            sizes[obj] = req.headers["content-length"]
+    return sizes
+
+
+def prepare_package(sizes, args):
+    """
+    Prepares the bytes of the object to be send to each server
+    :param sizes: dictionary with the sizes of each objects
+    :param args: dictionary with objects and servers
+    :return: a dictionary with objects and list of bytes to be send
+    """
+    packages = {}
+    for obj in sizes:
+        package = []
+        value = 0
+        scope = int(sizes[obj])
+        while not value == scope:
+            sol = int(sizes[obj]) / len(args["servers"])
+            value += round(sol)
+            if value > scope:
+                value -= round(sol)
+                break
+            package.append(round(sol))
+        if not scope-value == 0:
+            package.append(scope-value)
+        packages[obj] = package
+    return packages
+
+
+def make_request(args, packages):
+    items_list = {}
+    for pack in packages:
+        last_size = 0
+        i = 0
+        aux_item = []
+        for size in packages[pack]:
+            next_size = last_size + size
+            headers = {"Range": "bytes=%d-%d" % (last_size, next_size)}
+            url = "%s/%s" % (args["servers"][i], pack)
+            req = requests.get(url, headers=headers).content
+            i += 1
+            aux_item.append(req)
+            last_size = next_size
+        item = "".join(map(str, aux_item))
+        items_list[pack] = item
+    return items_list
 
 
 if __name__ == "__main__":
-    print("MAIN")
-    main()
+    args = parser()
+    ping_alive(args)
+    sizes = get_sizes(args)
+    packages = prepare_package(sizes, args)
+    items = make_request(args, packages)
+    for item in items:
+        print(item, items[item])
+
